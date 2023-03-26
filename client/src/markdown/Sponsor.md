1451818c0e8c17ac88e83eaddeef61ffbca65a20
杨琼璞 <jeffygisgreat@gmail.com>
2023-03-26 14:04:59 +0800

    :necktie: update sponsor list & display style

--- client/src/markdown/Sponsor.md
+++ client/src/markdown/Sponsor.md
@@ -1,18 +1,21 @@
-亲爱的用户：非常感谢您对[素问智聊斋](/)的支持和信任。**素问智聊斋**：旨在提供便捷的 [ChatGPT](https://nicelinks.site/tags/ChatGPT) 访问体验；很自豪地跟各位朋友分享，项目的功能与用户都在发展壮大，可**为您学习和高效带来效率提升**。
+**亲爱的用户**：非常感谢您对[素问智聊斋](/)的支持和信任。**素问智聊斋**：旨在提供便捷的 [ChatGPT](https://nicelinks.site/tags/ChatGPT) 访问体验；很自豪地跟各位朋友分享，项目的功能与用户都在发展壮大，可**为您学习和高效带来效率提升**。
 
 但是，要让这个项目继续茁壮成长，需要相应的资金支持。因此，诚恳地邀请您考虑**赞助**这个项目。您的慷慨相助将直接用于项目的开发和维护，保证您能够始终享受到最好的服务和使用体验。
 
 或者您也可以把这份赞助当作是对我们团队的鼓励和支持，让我们更加有动力去不断改进和完善这个项目，以满足您的需求和期望。无论您赞助多少，[轩帅](https://nicejade.bio.link/)都衷心感激，并且会认真对待每一笔捐款，确保其用途正确且透明。
 
-最后，**再次感谢您的支持和信任**。“[天意怜幽草，人间重晚晴](https://forum.lovejade.cn/d/139)”；我们希望通过不懈努力，为您创造更美好的 ChatGPT 学习和交流体验。如果您愿意赞助这个项目，可以通过以下两种方式进行捐赠。期待着您的参与和反馈，共同创造更美好的未来。
+最后，**再次感谢您的支持和信任**。“[天意怜幽草，人间重晚晴](https://forum.lovejade.cn/d/139)”；我们希望通过不懈努力，为您创造更美好的 ChatGPT 学习和交流体验。如果您愿意赞助这个项目，可以通过如下方式进行赞助。期待着您的参与和反馈，共同创造更美好的未来。
 
 <div class="m-auto text-center">
-  <img style="margin: 0" width=251 src="https://nicelinks.site/static/img/reward_wexin.jpg"  alt="微信扫码赞助" />
-  <p class="!my-0 font-medium">微信扫码赞助</p>
-</div>
-<div class="m-auto text-center">
-  <img style="margin: 0" width=251 src="https://nicelinks.site/static/img/reward_zhifubao.jpg"  alt="支付宝扫码赞助" />
-  <p class="!my-0 font-medium">支付宝扫码赞助</p>
+  <img style="margin: 0" class="shadow-md" width=234 src="https://nicelinks.site/static/img/reward_wexin.jpg"  alt="微信扫码赞助" />
+  <p class="font-medium text-gray-600">微信扫码赞助</p>
 </div>
 
-如果您[《素问智聊斋》](/)有任何想法，欢迎移步[关于](#/about)页面，在评论区留下您宝贵的建议或意见。
+如果您[《素问智聊斋》](/)有任何想法，欢迎移步[关于页面](#/about)，在评论区留下您宝贵的建议或意见（当然，您也可以选择在赞助中`备注`说明）。
+
+<h2 class="md:text-base">赞助者名单</h2>
+
+| 日期📅     | 赞助方🙏   | 备注📝       | 金额💰 |
+| :--- | :--- | :--- | :---: |
+| 23.03.25 | 和光同尘 | 无备注       | 66 RMB |
+| 23.03.26 | m*g | from chatbot | 1 RMB  |
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -49,24 +49,28 @@ module.exports = {
               marginLeft: 0,
             },
             "div p": {
-              "margin-top": "0.5rem !important",
-              "margin-bottom": "0.5rem !important",
+              marginTop: "0.5rem !important",
+              marginBottom: "0.5rem !important",
             },
             "li p": {
-              "margin-top": "0 !important",
-              "margin-bottom": "0 !important",
+              marginTop: "0 !important",
+              marginBottom: "0 !important",
+            },
+            "h2": {
+              fontSize: "1.25rem",
             },
             "h2, h3": {
-              marginTop: 0
+              marginTop: "1.25rem"
             },
             "h4, h5, h6": {
-              fontSize: '1.25rem',
+              fontSize: "1.125rem",
+              marginTop: "1.125rem",
             },
             "pre code::before": {
-              "padding-left": "unset",
+              paddingLeft: "unset",
             },
             "pre code::after": {
-              "padding-right": "unset",
+              paddingRight: "unset",
             },
             img: {
               marginTop: "1rem",
@@ -77,8 +81,8 @@ module.exports = {
               textDecoration: 'none',
             },
             "pre": {
-              "border": "1px solid #e8e8e8",
-              "border-radius": "0.5rem",
+              border: "1px solid #e8e8e8",
+              borderRadius: "0.5rem",
             },
             "pre, code": {
               color: "#213547",
@@ -88,16 +92,24 @@ module.exports = {
               backgroundColor: "#f3f4f6",
               color: "#f59e0b",
               fontWeight: "400",
-              "border-radius": "0.25rem",
+              borderRadius: "0.25rem",
             },
             "code::before": {
               content: '""',
-              "padding-left": "0.25rem",
+              paddingLeft: "0.25rem",
             },
             "code::after": {
               content: '""',
-              "padding-right": "0.25rem",
+              paddingRight: "0.25rem",
             },
+            "table": {
+              "border-top-width": "1px",
+              "border-bottom-width": "1px"
+            },
+            "thead tr th": {
+              "padding-top": "0.5rem",
+              "padding-bottom": "0.5rem",
+            }
           },
         },
       },
